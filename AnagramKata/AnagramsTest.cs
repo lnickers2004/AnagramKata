377baeaa4559e21c2f1af7c46f987de49ceb322e
lnickers2004 <lnickers@austin.rr.com>
2013-09-01 15:46:30 -0500

    initial commit

---
+++ AnagramKata/AnagramsTest.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Anagrams
+{
+    [TestFixture]
+    public class AnagramTests
+    {
+        [Test]
+        public void NoCharacters()
+        {
+            var expected = new List<string> { "" };
+            Assert.That(Anagrams.Of(""), Is.EqualTo(expected));
+        }
+        [Test]
+        public void OneCharacter()
+        {
+            var expected = new List<string> { "A" };
+            Assert.That(Anagrams.Of("A"), Is.EqualTo(expected));
+        }
+        [Test]
+        public void TwoCharacters()
+        {
+            var expected = new List<string> { "AB", "BA" };
+            Assert.That(Anagrams.Of("AB"), Is.EqualTo(expected));
+        }
+        [Test]
+        public void ThreeCharacters()
+        {
+            var expected = new List<string> { "ABC", "ACB", "BAC", "BCA", "CAB", "CBA" };
+            Assert.That(Anagrams.Of("ABC"), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void CountOfAnagramsOfFourCharacters()
+        {
+            Assert.That(Anagrams.Of("ABCD").Count, Is.EqualTo(24));
+        }
+    }
+}
